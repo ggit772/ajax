3fb708c2e9ce836213f8ce5aecb6f3e4e324e7ef
unknown <miemail@dominio.com>
2021-10-07 22:02:41 -0500

    ajax_1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function entrar(){
+    var user =   $.trim($("#user").val());
+    var pass =   $.trim($("#pass").val());
+
+    //alert(user);
+    $.ajax({
+        url: 'php/consulta.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {usuario: user, contraseña: pass },
+        success: function(data){
+            alert('todo esta bn');
+        },
+        error: function(r){
+            alert('algo esta mal: '+ r);
+        }
+    })
+    
+
+}
---
+++ php/consulta.php
@@ -0,0 +1,9 @@
+<?php
+    $user = $_POST['usuario'];
+
+    if($user == 'ana'){
+        echo true;
+    }else{
+        echo false;
+    }
+?>
